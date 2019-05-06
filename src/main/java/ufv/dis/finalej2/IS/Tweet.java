2d8afb0caa0f883df92a51d7314fae7bdda09c91
inigo <inigosobrini@gmail.com>
2019-05-06 18:16:25 +0200

    1st commit

---
+++ src/main/java/ufv/dis/finalej2/IS/Tweet.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package ufv.dis.finalej2.IS;
+
+/**
+ * @author inigo
+ *
+ */
+public class Tweet {
+
+	/**
+	 * 
+	 */
+	public Tweet() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
